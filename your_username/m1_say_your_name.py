998769eba611263860a17e2ca18875557fc701d8
David Fisher <fisherds@rose-hulman.edu>
2017-09-10 00:45:38 -0400

    Initial import

---
+++ your_username/m1_say_your_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module is meant to be your first program that you've made for EV3.
+  Make the robot say your name and then beep.
+  Use the samples to learn the API calls needed (not much code to write)
+
+Authors: Dave Fisher and PUT_YOUR_NAME_HERE.
+"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+
+
+# ------------------------------------------------------------------
+# TODO: 2. Make the robot say "David Fisher can write E V 3 programs"
+#   But of course use YOUR NAME.
+#
+#   Use the samples to figure out the necessary lines of code.
+#     Hint: This program can be done in as little as 2 lines of code (1 import, 1 function call)
+#   Feel free to run it, change it, and run it again with any message you like.
+# ------------------------------------------------------------------
