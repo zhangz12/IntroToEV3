fcbbbc4c1690e1f59d8b057e4a6636608d3ccbb7
zhangz12 <zhangz12@rose-hulman.edu>
2017-12-07 11:30:13 -0500

    say my name

--- zhangz12/m1_say_your_name.py
+++ zhangz12/m1_say_your_name.py
@@ -17,3 +17,13 @@ Authors: Dave Fisher and PUT_YOUR_NAME_HERE.
 #   Feel free to run it, change it, and run it again with any message you like.
 #   This program is simple, but if you can do this, then you have the tools to do FAR more.
 # ------------------------------------------------------------------
+
+
+import ev3dev.ev3 as ev3
+
+def main():
+    print("---------------------------------------------------------------------------------------")
+    print("Say your name")
+    print("---------------------------------------------------------------------------------------")
+    ev3.Sound.speak("Hello World").wait()
+main()
